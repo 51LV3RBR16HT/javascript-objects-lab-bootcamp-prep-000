0b1e3efec585fb100c434f39afb38d42fd6280d5
Barbara Washington <barbara.m.washington@gmail.com>
2018-03-10 19:26:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  Object.assign({}, newObject)
+  Object.assign({}, newObject: key )
   return object
 }
 
